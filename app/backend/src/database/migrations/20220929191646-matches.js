0171a69adfdd75a8fd21a876c55ee641ca8b79d3
GusRond <gustavorondello99@outlook.com>
2022-09-29 16:23:56 -0300

    feat: table migrations

---
+++ app/backend/src/database/migrations/20220929191646-matches.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.createTable('Matches', {
+    id: { 
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    home_team: { 
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Teams',
+        key: 'id'
+      }
+    },
+    home_team_goals: { 
+      allowNull: true,
+      type: Sequelize.INTEGER
+    },
+    away_team: { 
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Teams',
+        key: 'id'
+      }
+    },
+    away_team_goals: { 
+      allowNull: true,
+      type: Sequelize.INTEGER
+    },
+    in_progress: { 
+      allowNull: false,
+      type: Sequelize.BOOLEAN
+    },
+   });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
