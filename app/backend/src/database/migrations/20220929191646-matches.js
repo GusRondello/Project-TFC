0eca1c0792d6448fe94465dad40cf0dfbdecffa9
GusRond <gustavorondello99@outlook.com>
2022-09-29 19:00:58 -0300

    feat: models and migrations

--- app/backend/src/database/migrations/20220929191646-matches.js
+++ app/backend/src/database/migrations/20220929191646-matches.js
@@ -15,7 +15,7 @@ module.exports = {
       onUpdate: 'CASCADE',
       onDelete: 'CASCADE',
       references: {
-        model: 'Teams',
+        model: 'teams',
         key: 'id'
       }
     },
@@ -29,7 +29,7 @@ module.exports = {
       onUpdate: 'CASCADE',
       onDelete: 'CASCADE',
       references: {
-        model: 'Teams',
+        model: 'teams',
         key: 'id'
       }
     },
---
+++ app/backend/src/database/models/matchesModel.ts
@@ -0,0 +1,51 @@
+import { BOOLEAN, INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+import Teams from './teamsModel';
+
+class Matches extends Model {
+  id: number;
+  homeTeam: string;
+  homeTeamGoals: number;
+  awayTeam: string;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: true,
+  },
+  awayTeam: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: true,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+Matches.hasMany(Teams, { foreignKey: 'teamName', as: 'homeTeam' });
+Matches.hasMany(Teams, { foreignKey: 'teamName', as: 'awayTeam' });
+
+export default Matches;
